812b6da7f58397beae5ecf9e1a34aacda7f19a83
Carolina Di Julio <carolinadijulio@gmail.com>
2025-05-07 14:47:30 +0200

    cambios

--- useraddons/mi_proyecto/models/mi_proyecto.py
+++ useraddons/mi_proyecto/models/mi_proyecto.py
@@ -8,3 +8,8 @@ class MiProyecto(models.Model):
     descripcion = fields.Text(string='Descripción') 
     tarea_ids = fields.One2many('proyecto.tareas', 'proyecto_id', string='Tareas')
     etapa_ids = fields.One2many('proyecto.etapas', 'proyecto_id', string='Etapas')
+    tarea_contador = fields.Integer(string="Cantidad de Tareas", compute='_compute_tarea_contador')
+
+    def _compute_tarea_contador(self):
+        for record in self:
+            record.tarea_contador = len(record.tarea_ids)
