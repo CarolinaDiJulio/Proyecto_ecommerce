0acc5473d9df9fa3f2b50ed8fae49e42e463528f
Carolina Di Julio <carolinadijulio@gmail.com>
2025-05-12 14:24:56 +0200

    pruebas mejoradas

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,84 +1,45 @@
 describe('Prueba de inicio de sesión en Odoo', () => {
-  it('Debería iniciar sesión correctamente con credenciales válidas', () => {
-    // Visitar la página de inicio de sesión de Odoo
+  
+  // Esta función se ejecutará antes de cada test (it())
+  beforeEach(() => {
+    // Llamar al comando login personalizado
     cy.visit('http://localhost:8069/web?db=productos'); // Reemplaza con la URL de tu Odoo
 
-    // Llenar el campo de usuario
-    cy.get('input[name="login"]').type('sopanasiuk@d2plus.com'); // Reemplaza con el nombre de tu campo de usuario y credenciales
+     // Llenar el campo de usuario
+     cy.get('input[name="login"]').type('sopanasiuk@d2plus.com'); // Reemplaza con el nombre de tu campo de usuario y credenciales
 
-    // Llenar el campo de contraseña
-    cy.get('input[name="password"]').type('Odoo2025_'); // Reemplaza con el nombre de tu campo de contraseña
+     // Llenar el campo de contraseña
+     cy.get('input[name="password"]').type('Odoo2025_'); // Reemplaza con el nombre de tu campo de contraseña
 
-    // Hacer clic en el botón de inicio de sesión
-    cy.get('button.btn.btn-primary').click(); // Asegúrate de que este selector apunte al botón correcto
+     // Hacer clic en el botón de inicio de sesión
+     cy.get('button.btn.btn-primary').click(); // Asegúrate de que este selector apunte al botón correcto
+  });
 
-    cy.contains('Iniciar sesión').should('not.exist')
+  it('Debería iniciar sesión correctamente con credenciales válidas', () => {
+    cy.contains('Iniciar sesión').should('not.exist');
   });
 
   /////////////////////////////////////////////////////////////////////
   // cargar productos en la url
+  
   it('Debería cargar la página de productos correctamente después de iniciar sesión y verificar la URL', () => {
-    //inicia sesion 
-    cy.visit('http://localhost:8069/web?db=productos'); // Reemplaza con la URL de tu Odoo
- 
-    cy.get('input[name="login"]').type('sopanasiuk@d2plus.com'); // Reemplaza con el nombre de tu campo de usuario y credenciales
- 
-    cy.get('input[name="password"]').type('Odoo2025_'); // Reemplaza con el nombre de tu campo de contraseña
-
-    // Hacer clic en el botón de inicio de sesión
-    cy.get('button.btn.btn-primary').click(); 
-
-    // Esperar a que la URL de la página de productos esté disponible
-    cy.visit('http://localhost:8069/mitienda/productos')
- 
-    cy.contains('productos').should('exist'); // Verifica que el texto 'productos' esté en la página
+    cy.visit('http://localhost:8069/mitienda/productos');
+    cy.contains('productos').should('exist');
   });
 
   /////////////////////////////////////////////////////////////////////
   // filtrar por precio
-
   it('Debería filtrar productos por "Menos de 20€"', () => {
-    //inicia sesion 
-    cy.visit('http://localhost:8069/web?db=productos'); // Reemplaza con la URL de tu Odoo
-
-    cy.get('input[name="login"]').type('sopanasiuk@d2plus.com'); // Reemplaza con el nombre de tu campo de usuario y credenciales
- 
-    cy.get('input[name="password"]').type('Odoo2025_'); // Reemplaza con el nombre de tu campo de contraseña
-
-    // Hacer clic en el botón de inicio de sesión
-    cy.get('button.btn.btn-primary').click(); 
-
-    // Esperar a que la URL de la página de productos esté disponible
-    cy.visit('http://localhost:8069/mitienda/productos')
-    
-    // Seleccionar el filtro "Menos de 20€" en el formulario
-    cy.get('select[name="precio"]')
-      .select('Menos de 20€');  // Asegúrate de que el texto coincida con el valor del filtro
-
+    cy.visit('http://localhost:8069/mitienda/productos');
+    cy.get('select[name="precio"]').select('Menos de 20€');
     cy.get('.card-body').should('have.length', 3);
-   });
+  });
 
   /////////////////////////////////////////////////////////////////////
   // filtrar por categoria
-
-   it('Debería filtrar productos por categoria perifericos', () => {
-    //inicia sesion 
-    cy.visit('http://localhost:8069/web?db=productos'); // Reemplaza con la URL de tu Odoo
-
-    cy.get('input[name="login"]').type('sopanasiuk@d2plus.com'); // Reemplaza con el nombre de tu campo de usuario y credenciales
- 
-    cy.get('input[name="password"]').type('Odoo2025_'); // Reemplaza con el nombre de tu campo de contraseña
-
-    // Hacer clic en el botón de inicio de sesión
-    cy.get('button.btn.btn-primary').click(); 
-
-    // Esperar a que la URL de la página de productos esté disponible
-    cy.visit('http://localhost:8069/mitienda/productos')
-    
-    // Seleccionar el filtro "Menos de 20€" en el formulario
-    cy.get('select[name="categoria"]')
-      .select(1);  // Asegúrate de que el texto coincida con el valor del filtro
-
-    cy.get('.card-body').should('have.length', 2);
-   });
+  it('Debería filtrar productos por categoria ordenador', () => {
+    cy.visit('http://localhost:8069/mitienda/productos');
+    cy.get('select[name="categoria"]').select(2);
+    cy.get('.card-body').should('have.length', 1);
+  });
 });
