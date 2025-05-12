274dfbee5167482989331b44be04951efcbf38fa
Carolina Di Julio <carolinadijulio@gmail.com>
2025-05-12 14:14:04 +0200

    prueba filtro por categoria

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -14,6 +14,9 @@ describe('Prueba de inicio de sesión en Odoo', () => {
 
     cy.contains('Iniciar sesión').should('not.exist')
   });
+
+  /////////////////////////////////////////////////////////////////////
+  // cargar productos en la url
   it('Debería cargar la página de productos correctamente después de iniciar sesión y verificar la URL', () => {
     //inicia sesion 
     cy.visit('http://localhost:8069/web?db=productos'); // Reemplaza con la URL de tu Odoo
@@ -30,6 +33,10 @@ describe('Prueba de inicio de sesión en Odoo', () => {
  
     cy.contains('productos').should('exist'); // Verifica que el texto 'productos' esté en la página
   });
+
+  /////////////////////////////////////////////////////////////////////
+  // filtrar por precio
+
   it('Debería filtrar productos por "Menos de 20€"', () => {
     //inicia sesion 
     cy.visit('http://localhost:8069/web?db=productos'); // Reemplaza con la URL de tu Odoo
@@ -49,5 +56,29 @@ describe('Prueba de inicio de sesión en Odoo', () => {
       .select('Menos de 20€');  // Asegúrate de que el texto coincida con el valor del filtro
 
     cy.get('.card-body').should('have.length', 3);
+   });
+
+  /////////////////////////////////////////////////////////////////////
+  // filtrar por categoria
+
+   it('Debería filtrar productos por categoria perifericos', () => {
+    //inicia sesion 
+    cy.visit('http://localhost:8069/web?db=productos'); // Reemplaza con la URL de tu Odoo
+
+    cy.get('input[name="login"]').type('sopanasiuk@d2plus.com'); // Reemplaza con el nombre de tu campo de usuario y credenciales
+ 
+    cy.get('input[name="password"]').type('Odoo2025_'); // Reemplaza con el nombre de tu campo de contraseña
+
+    // Hacer clic en el botón de inicio de sesión
+    cy.get('button.btn.btn-primary').click(); 
+
+    // Esperar a que la URL de la página de productos esté disponible
+    cy.visit('http://localhost:8069/mitienda/productos')
+    
+    // Seleccionar el filtro "Menos de 20€" en el formulario
+    cy.get('select[name="categoria"]')
+      .select(1);  // Asegúrate de que el texto coincida con el valor del filtro
+
+    cy.get('.card-body').should('have.length', 2);
    });
 });
